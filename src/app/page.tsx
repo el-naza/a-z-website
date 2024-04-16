be9df6127cae33aca1766fe9f814a9e350f89afb
Emmanuel Ngene <emmanuelchinazangene@gmail.com>
2024-04-16 14:49:25 +0100

    chore: ignore unescaped entities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 
 export default function Home() {
@@ -339,50 +340,3 @@ function WordingItem({
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
