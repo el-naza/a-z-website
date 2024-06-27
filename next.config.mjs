9594f770c3bbade80d171ab4d77cc1993a00540a
Emmanuel Ngene <emmanuelchinazangene@gmail.com>
2024-06-27 20:22:38 +0100

    fix: add image loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    images: {
+        // remotePatterns: [
+        //   {
+        //     hostname: 'localhost',
+        //     pathname: '**',
+        //     port: '3000',
+        //     protocol: 'http',
+        //   },
+        // ],
+        loader: 'custom',
+        loaderFile: './loader.ts',
+    },
 };
 
 export default nextConfig;
