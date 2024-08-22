026a871e75c238c37c810db74c2b27783ed4310e
Emmanuel Ngene <emmanuelchinazangene@gmail.com>
2024-08-22 09:41:44 +0100

    feat: remove consultancy form

--- src/app/components/ConsultUs.tsx
+++ src/app/components/ConsultUs.tsx
@@ -5,86 +5,80 @@ import Image from "next/image";
 import { usePathname } from "next/navigation";
 
 export function ConsultUs() {
-  const pathname = usePathname();
-
-  if (pathname.includes("career") || pathname.includes("application-form"))
-    return false;
-
-  return (
-    <div className="relative flex justify-center w-full">
-      <div id="consult-us" className="absolute top-[-120px]" />
-      <div className="flex max-md:flex-col flex-wrap bg-white gap-[60px] gap-y-[16px] md:m-auto md:mb-[147px] mb-[147px] max-w-[1010px] max-md:px-0 px-[40px] py-[32px] rounded-[36px] md:rounded-[74px] items-center shadow-[0px_1px_22.8px_0px_#00000012] justify-center">
-        <Image
-          src="/customer-care-rep.png"
-          alt="customer care representative"
-          width={272}
-          height={620}
-          className="max-md:w-[145px] max-md:h-[299px] rounded-[700px] shadow-[0px_0px_42.1px_0px_#0000001A]"
-        />
-
-        <div className="flex-1 max-md:text-center">
-          <p className="font-anybody bg-gradient-to-r from-[#50E4ED] to-[#3492D6] text-[transparent] bg-clip-text text-[64px] leading-[76.8px] mb-[4px] max-md:text-[24px] max-md:leading-[28.8px] max-md:font-bold">
-            Consult Us
-          </p>
-          <p className="mb-[20px]">
-            Let's discuss your needs. We're
-            <br className="md:hidden" /> here to help.
-          </p>
-          <iframe
-            src="https://docs.google.com/forms/d/e/1FAIpQLScYoBZ_0Jbm5gHU1VX-zieRtdM15SOehytpMgzUSeZvqLG5mg/viewform?embedded=true"
-            width="100%"
-            height="1050"
-          >
-            Loading…
-          </iframe>
-          {/*<div className="flex flex-col gap-[20px]">
-              <CustomInput placeholder="Full name" />
-              <CustomInput type="email" placeholder="Email address" />
-              <CustomInput placeholder="Enter coupon" />
-              <CustomInput type="email" placeholder="Confirm email address" />
-              <CustomTextArea
-                name=""
-                id=""
-                rows={7.2}
-                placeholder="Write your message here"
-              />
-            </div>
-            <div className="flex flex-col pt-[16px] gap-[20px]">
-              <Image
-                src="/send.svg"
-                alt="send"
-                width={80}
-                height={80}
-                className="self-end max-md:w-[60px]"
-              />
-            </div>*/}
-        </div>
-      </div>
-
-      <Image
-        src="/Ellipse.svg"
-        alt="ellipse"
-        width={94}
-        height={94}
-        className="absolute -top-[75px] -left-[38px] -z-[10] max-md:-top-[60px] max-md:h-[60px] max-md:w-[60px] max-md:-left-[35px]"
-      />
-
-      <Image
-        src="/measure.svg"
-        alt="measure"
-        width={54}
-        height={94}
-        className="absolute top-[60px] -right-[36.7px] -z-[10] max-md:top-[60px] max-md:h-[50px] max-md:w-[50px] max-md:-right-[35px]"
-      />
-
-      <Image
-        src="/Ellipse1.svg"
-        alt="ellipse"
-        width={94}
-        height={94}
-        className="absolute top-[165px] -right-[36.7px] -z-[10] max-md:top-[120px] max-md:h-[60px] max-md:w-[60px] max-md:-right-[35px]"
-        // className="absolute top-[165px] -right-[36.7px] -z-[10] max-md:top-[120px] max-md:h-[60px] max-md:w-[60px] max-md:-right-[70px]"
-      />
-    </div>
-  );
+  // const pathname = usePathname();
+  // if (pathname.includes("career") || pathname.includes("application-form"))
+  //   return false;
+  // return (
+  //   <div className="relative flex justify-center w-full">
+  //     <div id="consult-us" className="absolute top-[-120px]" />
+  //     <div className="flex max-md:flex-col flex-wrap bg-white gap-[60px] gap-y-[16px] md:m-auto md:mb-[147px] mb-[147px] max-w-[1010px] max-md:px-0 px-[40px] py-[32px] rounded-[36px] md:rounded-[74px] items-center shadow-[0px_1px_22.8px_0px_#00000012] justify-center">
+  //       <Image
+  //         src="/customer-care-rep.png"
+  //         alt="customer care representative"
+  //         width={272}
+  //         height={620}
+  //         className="max-md:w-[145px] max-md:h-[299px] rounded-[700px] shadow-[0px_0px_42.1px_0px_#0000001A]"
+  //       />
+  //       <div className="flex-1 max-md:text-center">
+  //         <p className="font-anybody bg-gradient-to-r from-[#50E4ED] to-[#3492D6] text-[transparent] bg-clip-text text-[64px] leading-[76.8px] mb-[4px] max-md:text-[24px] max-md:leading-[28.8px] max-md:font-bold">
+  //           Consult Us
+  //         </p>
+  //         <p className="mb-[20px]">
+  //           Let's discuss your needs. We're
+  //           <br className="md:hidden" /> here to help.
+  //         </p>
+  //         <iframe
+  //           src="https://docs.google.com/forms/d/e/1FAIpQLScYoBZ_0Jbm5gHU1VX-zieRtdM15SOehytpMgzUSeZvqLG5mg/viewform?embedded=true"
+  //           width="100%"
+  //           height="1050"
+  //         >
+  //           Loading…
+  //         </iframe>
+  //         {/*<div className="flex flex-col gap-[20px]">
+  //             <CustomInput placeholder="Full name" />
+  //             <CustomInput type="email" placeholder="Email address" />
+  //             <CustomInput placeholder="Enter coupon" />
+  //             <CustomInput type="email" placeholder="Confirm email address" />
+  //             <CustomTextArea
+  //               name=""
+  //               id=""
+  //               rows={7.2}
+  //               placeholder="Write your message here"
+  //             />
+  //           </div>
+  //           <div className="flex flex-col pt-[16px] gap-[20px]">
+  //             <Image
+  //               src="/send.svg"
+  //               alt="send"
+  //               width={80}
+  //               height={80}
+  //               className="self-end max-md:w-[60px]"
+  //             />
+  //           </div>*/}
+  //       </div>
+  //     </div>
+  //     <Image
+  //       src="/Ellipse.svg"
+  //       alt="ellipse"
+  //       width={94}
+  //       height={94}
+  //       className="absolute -top-[75px] -left-[38px] -z-[10] max-md:-top-[60px] max-md:h-[60px] max-md:w-[60px] max-md:-left-[35px]"
+  //     />
+  //     <Image
+  //       src="/measure.svg"
+  //       alt="measure"
+  //       width={54}
+  //       height={94}
+  //       className="absolute top-[60px] -right-[36.7px] -z-[10] max-md:top-[60px] max-md:h-[50px] max-md:w-[50px] max-md:-right-[35px]"
+  //     />
+  //     <Image
+  //       src="/Ellipse1.svg"
+  //       alt="ellipse"
+  //       width={94}
+  //       height={94}
+  //       className="absolute top-[165px] -right-[36.7px] -z-[10] max-md:top-[120px] max-md:h-[60px] max-md:w-[60px] max-md:-right-[35px]"
+  //       // className="absolute top-[165px] -right-[36.7px] -z-[10] max-md:top-[120px] max-md:h-[60px] max-md:w-[60px] max-md:-right-[70px]"
+  //     />
+  //   </div>
+  // );
 }
